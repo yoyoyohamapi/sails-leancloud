3cb5c0fd658e03d7d3247172dedef02c468053b3
yoyoyohamapi <472285740@qq.com>
2015-08-03 10:52:15 +0800

    First init

--- config/leancloud.js
+++ config/leancloud.js
@@ -1,6 +1,6 @@
 // leancloud 相应配置
 module.exports.leancloud = {
-    APP_ID : 'aa', // your app id
-    APP_KEY : 'aa', // your app key
-    MASTER_KEY :'aa' // your app master key
+    APP_ID : '', // your app id
+    APP_KEY : '', // your app key
+    MASTER_KEY :'' // your app master key
 };
